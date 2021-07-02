5c2cecb03a516426ddc96468229b3239c1eba408
jimosk <jimosdk@gmail.com>
2021-07-02 18:02:04 +0300

    Implemented Replies::find_by_user_id,::find_by_question_id,#author,#question,#parent_reply,#child_replies

--- replies.rb
+++ replies.rb
@@ -11,7 +11,32 @@ class Reply
             WHERE
                 id = ?
         SQL
+        return nil if data.empty?
         Reply.new(data.first)
+    end
+
+    def self.find_by_author_id(author_id)
+        data = QuestionsDatabase.instance.execute(<<-SQL,author_id)
+            SELECT *
+            FROM
+                replies
+            WHERE
+                author_id = ?
+        SQL
+        return nil if data.empty?
+        data.map{|datum| Reply.new(datum)}
+    end
+
+    def self.find_by_subject_id(subject_id)
+        data = QuestionsDatabase.instance.execute(<<-SQL,subject_id)
+            SELECT *
+            FROM
+                replies
+            WHERE
+                subject_id = ?
+        SQL
+        return nil if data.empty?
+        data.map{|datum| Reply.new(datum)}
     end
 
     def initialize(options)
@@ -20,5 +45,29 @@ class Reply
         @author_id = options['author_id']
         @subject_id = options['subject_id']
         @parent_id = options['parent_id']
+    end
+
+    def author
+        User.find_by_id(author_id)
+    end
+
+    def question
+        Question.find_by_id(subject_id)
+    end
+
+    def parent_reply
+        Reply.find_by_id(parent_id)
+    end
+
+    def child_replies
+        data = QuestionsDatabase.instance.execute(<<-SQL,id: id)
+            SELECT *
+            FROM
+                replies
+            WHERE
+                parent_id = :id
+        SQL
+        return nil if data.empty?
+        data.map{|datum| Reply.new(datum)}
     end
 end
