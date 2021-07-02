9b940998648ec564bfd8bcd58ccec474a3a63857
jimosk <jimosdk@gmail.com>
2021-07-02 18:00:39 +0300

    Implemented Questio::find_by_author_id,#author,#replies

--- questions.rb
+++ questions.rb
@@ -12,7 +12,20 @@ class Question
             WHERE
                 id = ?
         SQL
+        return nil if data.empty?
         Question.new(data.first)
+    end
+
+    def self.find_by_author_id(author_id)
+        data = QuestionsDatabase.instance.execute(<<-SQL,author_id)
+            SELECT *
+            FROM
+                questions
+            WHERE
+                author_id = ?
+        SQL
+        return nil if data.empty?
+        data.map{|datum| Question.new(datum)}
     end
 
     def initialize(options)
@@ -22,4 +35,11 @@ class Question
         @author_id = options['author_id']
     end
 
+    def author
+        User.find_by_id(author_id)
+    end
+
+    def replies
+        Reply.find_by_subject_id(id)
+    end
 end
